321dbc24c5f2e0e987424ec6ee2ad28c1c089141
Pham Van Toan <toan.pv174268@sis.hust.edu.vn>
2022-05-02 17:20:36 +0700

    Build structure folder

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan')
+const exhbs = require('express-handlebars');
+const { json } = require('express/lib/response');
+const handlebars = exhbs.engine;
+const app = express();
+const PORT = 3000;
+
+//Set static path
+app.use(express.static(path.join(__dirname)));
+//HTTP logger
+app.use(morgan('combined'));
+//Template engine handlebars
+app.engine('.hbs',handlebars({extname: '.hbs'}));
+app.set('view engine','.hbs');
+app.set('views', path.join(__dirname,'resources','views'));
+console.log(__dirname);
+app.listen(PORT);
+app.get("/",function(req, res){
+    res.render('home');
+    }
+)
